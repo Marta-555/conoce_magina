be6188ef49dcab3f74a2f51b0823e95a253c0ee7
Marta García Ortega <mgarort-555@g.educaand.es>
2022-06-20 17:22:06 +0200

    Show info in datatable

--- public/js/datatable.js
+++ public/js/datatable.js
@@ -5,30 +5,41 @@ $(document).ready(function () {
         type:       'POST',
         dataType:   'json',
         success: function(data) {
-
             $.each(data, function (i, data) {
-                console.log(data.fecha_publicacion.date);
+                console.log(data);
                 var body = "<tr>";
+                body    += "<td class='details-control' id='modalInfo_"+ data.id +"'><i class='fas fa-plus-circle'></i></td>";
                 body    += "<td>" + data.titulo + "</td>";
-                body    += "<td>" + data.descripcion + "</td>";
                 body    += "<td>" + data.dificultad + "</td>";
                 body    += "<td>" + data.longitud + "</td>";
                 body    += "<td>" + data.tiempo + "</td>";
-                body    += "<td>" + data.mapa + "</td>";
                 body    += "<td>" + data.desnivel + "</td>";
                 body    += "<td><img src='" + data.image + "'</td>";
                 body    += "<td>" + data.municipio + "</td>";
                 body    += "<td>" + data.tipoRuta + "</td>";
-                body    += "<td>" + data.fecha_publicacion.date + "</td>";
 
-                if(data.puntoInteres != null){
-                    body    += "<td>" + data.puntoInteres + "</td>";
-                }
+                // if(data.puntoInteres != null){
+                //     body    += "<td>" + data.puntoInteres + "</td>";
+                // }
                 body    += "<td><button class='btn' id='editar'><i class='fas fa-pencil-alt'></i></button><button class='btn' id='borrar'><i class='fas fa-trash-alt'></i></button></td>";
                 body    += "</tr>";
                 $('#tableUsuario tbody').append(body);
+
+                var datos = informacion(data);
+                $('#ventana').append(datos)
             });
-            $('#tableUsuario').DataTable({
+
+            var dt = $('#tableUsuario').DataTable();
+
+            $('#tableUsuario tbody').on('click',  'tr td.details-control', function(){
+                var pulsado = $(this).attr("id");
+
+                var elemento = '#ventana #'+pulsado;
+                $(elemento).addClass('ver');
+
+                $('#cerrar').on('click', function(){
+                    $(elemento).removeClass('ver');
+                })
 
             });
         },
@@ -36,5 +47,43 @@ $(document).ready(function () {
             alert('Ajax request failed.');
         }
     });
-
 });
+
+
+function informacion(data) {
+    return (
+        '<div class="col-sm-12 panel"  id="modalInfo_'+ data.id +'">\
+            <div class="informacion">\
+                <i class="far fa-times-circle" id="cerrar"></i>\
+                <div class="row landing-image-text">\
+                    <div class="col-md-6 col-md-push-6">\
+                        <img class="center-block" src="' + data.image + '" alt="imagen ruta"> <br>\
+                        <div>' + data.mapa + '</div>\
+                    </div>\
+                    <div class="col-md-6 col-md-pull-6">\
+                        <h2 class="font-alt">' + data.titulo + '</h2>\
+                        <p><strong>Dificultad: </strong>' + data.dificultad + '&nbsp; &nbsp; &nbsp; &nbsp; <strong>Longitud: </strong>' + data.longitud + ' Km <br>\
+                        <strong>Tiempo: </strong>' + data.tiempo + ' horas &nbsp; &nbsp; &nbsp; &nbsp; <strong>Desnivel: </strong>' + data.desnivel + '<br>\
+                        <strong>Tipo de ruta:  </strong>' + data.tipoRuta + '&nbsp; &nbsp; &nbsp; &nbsp; <strong>Punto de Interés: </strong>' + data.puntoInteres +'</p>\
+                        <p>' + data.descripcion + '</p>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>'
+    );
+}
+
+
+
+ // <p>Título:  ' + data.titulo + '<br>\
+                // Descripción:  ' + data.descripcion + '<br>\
+                // Dificultad:  ' + data.dificultad + '<br>\
+                // Longitud:  ' + data.longitud + '<br>\
+                // Tiempo:  ' + data.tiempo + '<br>\
+                // Mapa:  ' + data.mapa + '<br>\
+                // // Desnivel:  ' + data.desnivel + '<br>\
+                // {/* Image:  ' + data.image + '<br>\ */}
+                // Municipio:  ' + data.municipio + '<br>\
+                // TipoRuta:  ' + data.tipoRuta + '<br>\
+                // Publicacion:  ' + data.fecha_publicacion.date + '<br>\
+                // </p>\
